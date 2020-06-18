f0171b757ae031ab57b85b45b3fc7094a6422192
ajstewartlang <andrew.stewart@manchester.ac.uk>
2020-06-18 13:42:37 +0100

    general update

--- script/ufo_script.R
+++ script/ufo_script.R
@@ -1,8 +1,12 @@
-# RUM Virtual Meeting 18/03/2020
 # Working with the Tidy Tuesday UFO dataset
 
-library(tidyverse) # behold all things tidy
-library(openintro) # useful functions such as abb2state
+# Never install packages in your script - but you can copy thefollowing four
+# lines of code and run them in your console - just delete the # from the start
+# install.packages("tidyverse")
+# install.packages("ggrepel")
+# install.packages("patchwork)
+
+library(tidyverse) # load the tidyverse
 library(ggrepel) # needed to ensure text labels don't overlap
 library(patchwork) # needed to combine our 4 plots at the end
 
@@ -12,8 +16,8 @@ ufo_sightings <- read_csv("https://raw.githubusercontent.com/rfordatascience/tid
 # bar plot of top 10 US states with number of sightings in each state
 # the openintro::abbr2state() function converts state abbreviations to full names
 plot1 <- ufo_sightings %>%
-  mutate(state = abbr2state(state)) %>%
   filter(!is.na(state)) %>%
+  mutate(state = str_to_upper(state)) %>%
   group_by(state) %>%
   tally() %>%
   top_n(10) %>%
@@ -30,7 +34,6 @@ plot1 <- ufo_sightings %>%
 
 # work out the top 10 states with UFO sightings
 top_states <- ufo_sightings %>%
-  mutate(state = abbr2state(state)) %>%
   filter(!is.na(state)) %>%
   group_by(state) %>%
   tally() %>%
@@ -40,8 +43,7 @@ top_states <- ufo_sightings %>%
 # work out states within lat and long limits (i.e., exclude Alaska)
 tidied_ufo <- ufo_sightings %>%
   filter(country == "us") %>%
-  filter(latitude > 24 & latitude < 50) %>%
-  mutate(state = abbr2state(state))
+  filter(latitude > 24 & latitude < 50)
 
 # plot all sightings on a map of the US, with 10 top states coloured
 plot2 <- tidied_ufo %>%
@@ -51,11 +53,12 @@ plot2 <- tidied_ufo %>%
   coord_cartesian() +
   labs(title = "Sites of UFO Sightings in the US") +
   guides(colour = FALSE) +
-  guides(fill = FALSE)
+  guides(fill = FALSE) +
+  theme(text = element_text(size = 15))
 
 # bar plot of top 10 UFO shapes spotted in California
 plot3 <- tidied_ufo %>%
-  filter(state == "California") %>%
+  filter(state == "ca") %>%
   filter(ufo_shape != "other") %>%
   filter(ufo_shape != "unknown") %>%
   group_by(ufo_shape) %>%
@@ -72,35 +75,7 @@ plot3 <- tidied_ufo %>%
   theme_minimal() +
   theme(text = element_text(size = 15))
 
-# Plot 4
-# read in dataset which contains lat and long of US states and cities
-uscities <- read_csv("data/uscities.csv")
-
-# extract data just about states
-states <- map_data("state")
-
-# create a data frame of the top 10 most populous cities in California
-big_cal <- uscities %>%
-  filter(state_name == "California") %>%
-  arrange(-population) %>%
-  top_n(10, population) 
-
-# plot UFO sightings in California and add points representing the 10 largest cities
-plot4 <- tidied_ufo %>%
-  filter(state == "California") %>%
-  ggplot(aes(x = longitude, y = latitude)) + 
-  geom_polygon(data = filter(states, region == "california"), 
-               aes(x = long, y = lat, fill = region, group = group), alpha = .2, color = "#D89000") +
-  geom_point(size = 2, alpha = .5, colour = "#D89000") +
-  geom_text_repel(data = big_cal, aes(x = lng, y = lat, label = city), size = 5) +
-  geom_point(data = big_cal, aes(x = lng, y = lat), size = 2, alpha = .5) +
-  guides(fill = FALSE) +
-  theme_void() +
-  coord_cartesian() +
-  labs(title = "Sites of UFO Sightings in California") +
-  labs(caption = "A subset of the Tidy Tuesday UFO Sightings around the world dataset")
-
 # Put plots together
-my_plot <- (plot1 + plot3) / (plot2 + plot4)
+my_plot <- (plot1 + plot3) / (plot2)
 
-ggsave("images/ufo_plot.jpg", plot = my_plot, width = 15, height = 10)
+ggsave("ufo_plot.jpg", plot = my_plot, width = 12, height = 10)
